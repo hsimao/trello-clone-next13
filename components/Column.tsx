16b52049bf3ce3afc2df4c5bafa6cb842043523a
hsimao <e087754958@gmail.com>
2023-09-02 23:37:10 +0800

    feat: Board, Column component

---
+++ components/Column.tsx
@@ -0,0 +1,83 @@
+import { PlusCircleIcon } from '@heroicons/react/24/solid'
+import { Draggable, Droppable } from 'react-beautiful-dnd'
+import TodoCard from './TodoCard'
+
+type Props = {
+  id: TypedColumn
+  todos: Todo[]
+  index: number
+}
+
+const idToColumnText: {
+  [key in TypedColumn]: string
+} = {
+  todo: 'To DO',
+  inprogress: 'In Progress',
+  done: 'Done'
+}
+
+function Column({ id, todos, index }: Props) {
+  return (
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {/* render droppable todos in the column */}
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`p-2 rounded-2xl shadow-sm ${
+                  snapshot.isDraggingOver ? 'bg-green-200' : 'bg-white/50'
+                }`}
+              >
+                <h2 className="flex justify-between font-bold text-xl p-2">
+                  {idToColumnText[id]}
+                  <span className="text-gray-500 bg-gray rounded-full px-2 py-2 text-sm">
+                    {todos.length}
+                  </span>
+                </h2>
+
+                <div className="space-y-2">
+                  {todos.map((todo, index) => (
+                    <Draggable
+                      key={todo.$id}
+                      draggableId={todo.$id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <TodoCard
+                          todo={todo}
+                          index={index}
+                          id={id}
+                          innerRef={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+
+                  {/* 拖曳進來時會自動挪出出空間 */}
+                  {provided.placeholder}
+
+                  <div className="flex items-end justify-end p-2">
+                    <button className="text-green-500 hover:text-green-600">
+                      <PlusCircleIcon className="h-10 w-10" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
+  )
+}
+
+export default Column
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import Image from 'next/image'
-import React from 'react'
-
 import Avatar from 'react-avatar'
 import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
